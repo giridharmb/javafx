8d01d723e8dd4cbb12ff6ceee93c6507c10d9a4c
Giridhar Bhujanga <gbhujanga@walmartlabs.com>
2024-11-28 11:33:06 -0800

    updated code , which generates DMG file which can then be installed on Mac OSX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,20 @@
 module org.example.gbhujanfx1 {
+    requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.net.http;
-    requires javafx.base;
     requires javafx.graphics;
-//    requires javafx.swing;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+
+    requires java.net.http; // Add this line
 
     exports org.example.gbhujanfx1;
     opens org.example.gbhujanfx1 to javafx.fxml;
--- src/main/java/org/example/gbhujanfx1/Application.java
+++ src/main/java/org/example/gbhujanfx1/Application.java
@@ -1,35 +1,61 @@
 // MainApplication.java
 package org.example.gbhujanfx1;
 
+import java.net.URI;
+
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import java.net.URI;
+
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 /*
+Final:
+
+mvn clean compile package javafx:jlink jpackage:jpackage
+
+Will Generate >>
+
+target/dist/MyApp-1.0.0.pkg
+
+Log >>
+
+[INFO] Using: /Users/giri/Library/Java/JavaVirtualMachines/openjdk-23.0.1/Contents/Home/bin/jpackage, major version: 23
+[INFO] jpackage options:
+[INFO]   --name MyApp
+[INFO]   --dest /Users/giri/git/java/javafx/target/dist
+[INFO]   --type pkg
+[INFO]   --app-version 1.0.0
+[INFO]   --runtime-image /Users/giri/git/java/javafx/target/gbhujanfx1
+[INFO]   --input /Users/giri/git/java/javafx/target
+[INFO]   --main-class org.example.gbhujanfx1.Application
+[INFO]   --main-jar gbhujanfx1-1.0-SNAPSHOT.jar
+*/
+
+/*
 mvn clean install
 
 mvn clean package
-
-java --module-path /Users/gbhujan/javafxsdk/javafx-sdk-23.0.1/lib --add-modules javafx.controls,javafx.fxml -jar target/gbhujanFX1-1.0-SNAPSHOT.jar
 */
 
 /*
+Run > Edit Configurations > Template = "Application"
 
-mvn javafx:jlink
+Run > Edit Configurations > VM Options >
 
-*/
+--module-path /Users/giri/javafx/javafx-sdk-23.0.1/lib --add-modules javafx.controls,javafx.fxml,javafx.graphics -jar target/gbhujanFX1-1.0-SNAPSHOT.jar --add-exports javafx.graphics/com.sun.javafx.sg.prism=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.scene=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.util=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.reflect=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.application=ALL-UNNAMED --add-exports javafx.graphics/com.sun.glass.ui=ALL-UNNAMED
 
+Main Class:
 
-/*
+org.example.gbhujanfx1.Application
 
---module-path /Users/gbhujan/javafxsdk/javafx-sdk-23.0.1/lib --add-modules javafx.controls,javafx.fxml -jar target/gbhujanFX1-1.0-SNAPSHOT.jar
+Working Directory >
 
+$PROJECT_DIR$
 */
 
 /*
@@ -46,9 +72,9 @@ src/
     └── resources/  # If you add resources later
 README.md  # Add installation/running instructions
 
+Query JavaFX Packages >>
 
-jar tf /Users/gbhujan/.m2/repository/org/example/gbhujanFX1/1.0-SNAPSHOT/gbhujanFX1-1.0-SNAPSHOT.jar
-
+jar tf /Users/giri/.m2/repository/org/example/gbhujanfx1/1.0-SNAPSHOT/gbhujanfx1-1.0-SNAPSHOT.jar|grep javafx
 */
 
 public class Application extends javafx.application.Application {
